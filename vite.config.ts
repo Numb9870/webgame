3432047cad8b7817bf911195e1e2148a3f916543
看不穿的是红尘 <fgy_game@163.com>
2025-03-13 16:54:23 +0800

    0.0.10 vite配置SSG/SSE启用naive-ui

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,5 @@
 import { defineConfig, loadEnv } from 'vite';
+import { setup } from '@css-render/vue3-ssr';
 import pluginInit from './vite/plugins';
 import serverInit from './vite/server';
 
@@ -12,5 +13,21 @@ export default defineConfig(({ command, mode }) => {
     base: env.VITE_APP_BASE_URL,
     plugins: pluginInit(),
     server: serverInit(env),
+    ssr: {
+      noExternal: ['naive-ui', 'vueuc', 'date-fns'],
+    },
+    ssgOptions: {
+      async onBeforePageRender(_, __, appCtx) {
+        const { collect } = setup(appCtx.app);
+        (appCtx as any).__collectStyle = collect;
+        return undefined;
+      },
+      async onPageRendered(_, renderedHTML, appCtx) {
+        return renderedHTML.replace(
+          /<\/head>/,
+          `${(appCtx as any).__collectStyle()}</head>`
+        );
+      },
+    },
   };
 });
