4096c382fcfa76642be806b1d230583cdd348c79
看不穿的是红尘 <fgy_game@163.com>
2025-03-13 17:09:27 +0800

    0.0.11 更改vue-router历史模式

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,9 +1,17 @@
-import { createRouter, createWebHistory } from 'vue-router';
-
+import {
+  createRouter,
+  createMemoryHistory,
+  createWebHistory,
+} from 'vue-router';
 import { constantRoutes } from './constantRouters';
 
+console.log(import.meta.env.MODE);
+
 export const router = createRouter({
-  history: createWebHistory(),
+  history:
+    import.meta.env.MODE == 'development'
+      ? createWebHistory()
+      : createMemoryHistory(),
   routes: constantRoutes,
   scrollBehavior: () => ({ left: 0, top: 0 }),
 });
